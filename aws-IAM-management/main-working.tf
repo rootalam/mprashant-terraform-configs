47c67dbe7a4d650802efda3682f9edce5fe096ca
rootalam <88923083+rootalam@users.noreply.github.com>
2025-03-15 20:00:20 +0530

    Create main-working.tf

---
+++ aws-IAM-management/main-working.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"  
+}
+
+locals {
+  users_data = yamldecode(file("./users.yaml")).users
+
+  user_role_pair = flatten([for user in local.users_data: [for role in user.roles: {
+    username = user.username 
+    role = role
+  }] ])
+  
+}
+
+output "output1" {
+  value = local.user_role_pair
+}
+
+
+output "output" {
+  value = local.users_data[*].username
+}
+
+#Creating users
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_data[*].username)
+  name = each.value
+}
+
+
+#Password creation
+resource "aws_iam_user_login_profile" "profile" {
+  for_each = aws_iam_user.users
+  user = each.value.name
+  password_length = 12
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
+}
+
+# Attaching policies
+
+resource "aws_iam_policy_attachment" "main" {
+  name = "test"
+  for_each = {
+    for pair in local.user_role_pair :
+    "${pair.username}-${pair.role}" => pair 
+  }
+
+  users = [aws_iam_user.users[each.value.username].name]
+  policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
+}
